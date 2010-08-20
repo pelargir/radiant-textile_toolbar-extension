dbfbbe2dc8a43d1cebe966e894dff1cf25499023
Matthew <pelargir@gmail.com>
2010-08-20 00:03:29 -0400

    initial commit

---
+++ textile_toolbar_extension.rb
@@ -0,0 +1,22 @@
+begin
+  require_dependency 'application_controller'
+rescue MissingSourceFile
+  require_dependency 'application'
+end
+
+class TextileToolbarExtension < Radiant::Extension
+  version "1.0"
+  description "Adds toolbars to Textile-enabled text areas in the admin."
+  url "http://dev.radiantcms.org/radiant/browser/trunk/extensions/textile-toolbar"
+  
+  def activate
+    Admin::PageController.class_eval do
+      before_filter :set_textile_toolbar_javascript
+      def set_textile_toolbar_javascript
+        %w(lowpro textile_toolbar).each { |e| include_javascript e }
+      end
+    end
+  end
+end
+
+# SDG
